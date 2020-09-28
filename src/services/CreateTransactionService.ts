6924707ae5b35daf4f5f7ee3b8cfefb8fbae39b7
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2020-09-28 09:28:59 -0300

    desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,29 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute( { title, value, type }: Request ): Transaction {
+
+    if(!title)
+      throw new Error("The transaction title has not been set");
+    
+    if(!value)
+      throw new Error("The transaction value has not been set");
+    
+    if( ! ['income', 'outcome'].includes(type) )
+      throw new Error("The transaction must have a type defined as income or outcome");
+
+    
+    const {total} = this.transactionsRepository.getBalance();
+
+    if(type == 'outcome' && value > total ){
+      throw new Error("You don't have enough balance"); 
+    }
+
+
+    const transaction = this.transactionsRepository.create({title, value, type});
+
+    return transaction;
+
   }
 }
 
